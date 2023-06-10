64c41912e6fe066ffffe2124d2372e8fccea6ce0
skunbydev <skunbydev@gmail.com>
2023-06-10 01:33:23 -0300

    buceta do CODIGO CERTO, DEPOIS DE PERDER 500 HORAS EU CONSEGUI

--- SkunbySchool/web/script.js
+++ SkunbySchool/web/script.js
@@ -1,212 +1,232 @@
 function exibirAviso(elemento, mensagem) {
     const avisoElemento = elemento.nextElementSibling;
     avisoElemento.textContent = mensagem;
-  }
-  
-  function limparAviso(elemento) {
+}
+
+function limparAviso(elemento) {
     const avisoElemento = elemento.nextElementSibling;
     avisoElemento.textContent = "";
-  }
-  
-  function validarNome() {
+}
+
+function validarNome() {
     const input = document.getElementById("nome");
     const nome = input.value;
     const regex = /^[a-zA-Z\s]+$/;
-  
+
     if (nome.length === 0) {
-      exibirAviso(input, "Por favor, insira um nome.");
-      input.setCustomValidity("Por favor, insira um nome.");
-      return false;
+        exibirAviso(input, "Por favor, insira um nome.");
+        input.setCustomValidity("Por favor, insira um nome.");
+        return false;
     } else if (!regex.test(nome)) {
-      exibirAviso(input, "Por favor, insira apenas letras.");
-      input.setCustomValidity("Por favor, insira apenas letras");
-      return false;
+        exibirAviso(input, "Por favor, insira apenas letras.");
+        input.setCustomValidity("Por favor, insira apenas letras");
+        return false;
     } else {
-      limparAviso(input);
-      input.setCustomValidity("");
-      return true;
+        limparAviso(input);
+        input.setCustomValidity("");
+        return true;
     }
-  }
-  
-  function validarMatricula() {
+}
+
+
+
+function validarMatricula() {
     const input = document.getElementById("matricula");
     let matricula = input.value;
-  
+
     if (matricula.length === 0) {
-      matricula = "FR";
+        matricula = "FR";
     } else if (!matricula.startsWith("FR")) {
-      matricula = "FR" + matricula;
+        matricula = "FR" + matricula;
     }
-  
+
     matricula = matricula.slice(0, 8);
-  
+
     input.value = matricula;
-  
+
     if (matricula.length === 0) {
-      exibirAviso(input, "Por favor, insira uma matrícula.");
-      input.setCustomValidity("Por favor, insira uma matrícula.");
-      return false;
+        exibirAviso(input, "Por favor, insira uma matrícula.");
+        input.setCustomValidity("Por favor, insira uma matrícula.");
+        return false;
     } else if (matricula.length < 8) {
-      exibirAviso(input, "Por favor, insira uma matrícula válida.");
-      input.setCustomValidity("Por favor, insira uma matrícula válida.");
-      return false;
+        exibirAviso(input, "Por favor, insira uma matrícula válida.");
+        input.setCustomValidity("Por favor, insira uma matrícula válida.");
+        return false;
     } else {
-      limparAviso(input);
-      input.setCustomValidity("");
-      return true;
+        limparAviso(input);
+        input.setCustomValidity("");
+        return true;
     }
-  }
-  
-  function validarCPF() {
+}
+
+function validarCPF() {
     const input = document.getElementById("cpf");
     let cpf = input.value.replace(/\D/g, "");
-  
+
     if (cpf.length !== 11) {
-      exibirAviso(input, "Por favor, insira um CPF válido.");
-      input.setCustomValidity("Por favor, insira um CPF válido.");
-      return false;
+        exibirAviso(input, "Por favor, insira um CPF válido.");
+        input.setCustomValidity("Por favor, insira um CPF válido.");
+        return false;
     } else {
-      limparAviso(input);
-      input.setCustomValidity("");
+        limparAviso(input);
+        input.setCustomValidity("");
     }
-  
+
     if (cpf.length >= 3) {
-      cpf = cpf.substring(0, 3) + "." + cpf.substring(3);
+        cpf = cpf.substring(0, 3) + "." + cpf.substring(3);
     }
     if (cpf.length >= 7) {
-      cpf = cpf.substring(0, 7) + "." + cpf.substring(7);
+        cpf = cpf.substring(0, 7) + "." + cpf.substring(7);
     }
     if (cpf.length >= 11) {
-      cpf = cpf.substring(0, 11) + "-" + cpf.substring(11);
+        cpf = cpf.substring(0, 11) + "-" + cpf.substring(11);
     }
-  
+
     input.value = cpf.substring(0, 14);
-  
+
     return true;
-  }
-  
-  
-  function validarTelefone() {
+}
+
+
+function validarTelefone() {
     const input = document.getElementById("telefone");
     let telefone = input.value.replace(/\D/g, "");
-  
+
     if (telefone.length < 11 || telefone.length > 12) {
-      exibirAviso(input, "Por favor, insira um número válido.");
-      input.setCustomValidity("Por favor, insira um número válido.");
-      return false;
+        exibirAviso(input, "Por favor, insira um número válido.");
+        input.setCustomValidity("Por favor, insira um número válido.");
+        return false;
     } else {
-      limparAviso(input);
-      input.setCustomValidity("");
+        limparAviso(input);
+        input.setCustomValidity("");
     }
-  
+
     if (telefone.length >= 2) {
-      telefone = "(" + telefone.substring(0, 2) + ") " + telefone.substring(2);
+        telefone = "(" + telefone.substring(0, 2) + ") " + telefone.substring(2);
     }
     if (telefone.length >= 7) {
-      telefone = telefone.substring(0, 6) + "." + telefone.substring(6);
+        telefone = telefone.substring(0, 6) + "." + telefone.substring(6);
     }
     if (telefone.length >= 12) {
-      telefone = telefone.substring(0, 11) + "-" + telefone.substring(11);
+        telefone = telefone.substring(0, 11) + "-" + telefone.substring(11);
     }
-  
+
     input.value = telefone.substring(0, 16);
-  
+
     return true;
-  }
-  
-  
-  function validarCep() {
+}
+
+function buscarEndereco(cep) {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then((response) => response.json())
+        .then((data) => {
+            const ruaInput = document.getElementById("rua");
+            const bairroInput = document.getElementById("bairro");
+
+            ruaInput.value = data.logradouro;
+            bairroInput.value = data.bairro;
+        })
+        .catch((error) => {
+            console.error("Erro ao buscar o endereço:", error);
+        });
+}
+
+
+function validarCep() {
     const input = document.getElementById("cep");
     let cep = input.value.replace(/\D/g, "");
-  
+
     if (cep.length !== 8) {
-      exibirAviso(input, "Por favor, insira um CEP válido.");
-      input.setCustomValidity("Por favor, insira um CEP válido.");
-      return false;
+        exibirAviso(input, "Por favor, insira um CEP válido.");
+        input.setCustomValidity("Por favor, insira um CEP válido.");
+        return false;
     } else {
-      limparAviso(input);
-      input.setCustomValidity("");
+        limparAviso(input);
+        input.setCustomValidity("");
     }
-  
+
     if (cep.length >= 5) {
-      cep = cep.substring(0, 5) + "-" + cep.substring(5);
+        cep = cep.substring(0, 5) + "-" + cep.substring(5);
     }
-  
+
     input.value = cep.substring(0, 9);
-  
+
+    // Realizar a busca do CEP e preencher bairro e rua
+    buscarEndereco(cep);
+
     return true;
-  }
-  
-  function validarRua() {
+}
+
+function validarRua() {
     const input = document.getElementById("rua");
     const rua = input.value;
-  
+
     if (rua.length === 0) {
-      exibirAviso(input, "Por favor, insira uma rua.");
-      input.setCustomValidity("Por favor, insira uma rua.");
-      return false;
+        exibirAviso(input, "Por favor, insira uma rua.");
+        input.setCustomValidity("Por favor, insira uma rua.");
+        return false;
     } else {
-      limparAviso(input);
-      input.setCustomValidity("");
-      return true;
+        limparAviso(input);
+        input.setCustomValidity("");
+        return true;
     }
-  }
-  
-  function validarNumero() {
+}
+
+function validarNumero() {
     const input = document.getElementById("numero");
     const numero = input.value;
-  
+
     if (numero.length === 0) {
-      exibirAviso(input, "Por favor, insira um número.");
-      input.setCustomValidity("Por favor, insira um número.");
-      return false;
+        exibirAviso(input, "Por favor, insira um número.");
+        input.setCustomValidity("Por favor, insira um número.");
+        return false;
     } else {
-      limparAviso(input);
-      input.setCustomValidity("");
-      return true;
+        limparAviso(input);
+        input.setCustomValidity("");
+        return true;
     }
-  }
-  
-  function validarBairro() {
+}
+
+function validarBairro() {
     const input = document.getElementById("bairro");
     const bairro = input.value;
-  
+
     if (bairro.length === 0) {
-      exibirAviso(input, "Por favor, insira um bairro.");
-      input.setCustomValidity("Por favor, insira um bairro.");
-      return false;
+        exibirAviso(input, "Por favor, insira um bairro.");
+        input.setCustomValidity("Por favor, insira um bairro.");
+        return false;
     } else {
-      limparAviso(input);
-      input.setCustomValidity("");
-      return true;
+        limparAviso(input);
+        input.setCustomValidity("");
+        return true;
     }
-  }
-  
-  const nomeInput = document.getElementById("nome");
-  nomeInput.addEventListener("input", validarNome);
-  
-  const matriculaInput = document.getElementById("matricula");
-  matriculaInput.addEventListener("input", validarMatricula);
-  
-  const cpfInput = document.getElementById("cpf");
-  cpfInput.addEventListener("input", validarCPF);
-  
-  const telefoneInput = document.getElementById("telefone");
-  telefoneInput.addEventListener("input", validarTelefone);
-  
-  const cepInput = document.getElementById("cep");
-  cepInput.addEventListener("input", validarCep);
-  
-  const ruaInput = document.getElementById("rua");
-  ruaInput.addEventListener("input", validarRua);
-  
-  const numeroInput = document.getElementById("numero");
-  numeroInput.addEventListener("input", validarNumero);
-  
-  const bairroInput = document.getElementById("bairro");
-  bairroInput.addEventListener("input", validarBairro);
-  
-  function verificarCampos(event) {
+}
+
+const nomeInput = document.getElementById("nome");
+nomeInput.addEventListener("input", validarNome);
+
+const matriculaInput = document.getElementById("matricula");
+matriculaInput.addEventListener("input", validarMatricula);
+
+const cpfInput = document.getElementById("cpf");
+cpfInput.addEventListener("input", validarCPF);
+
+const telefoneInput = document.getElementById("telefone");
+telefoneInput.addEventListener("input", validarTelefone);
+
+const cepInput = document.getElementById("cep");
+cepInput.addEventListener("input", validarCep);
+
+const ruaInput = document.getElementById("rua");
+ruaInput.addEventListener("input", validarRua);
+
+const numeroInput = document.getElementById("numero");
+numeroInput.addEventListener("input", validarNumero);
+
+const bairroInput = document.getElementById("bairro");
+bairroInput.addEventListener("input", validarBairro);
+
+function verificarCampos(event) {
     event.preventDefault();
     const nomeValido = validarNome();
     const matriculaValida = validarMatricula();
@@ -216,24 +236,23 @@ function exibirAviso(elemento, mensagem) {
     const ruaValida = validarRua();
     const numeroValido = validarNumero();
     const bairroValido = validarBairro();
-  
+
     if (
-      nomeValido &&
-      matriculaValida &&
-      cpfValido &&
-      telefoneValido &&
-      cepValido &&
-      ruaValida &&
-      numeroValido &&
-      bairroValido
+        nomeValido &&
+        matriculaValida &&
+        cpfValido &&
+        telefoneValido &&
+        cepValido &&
+        ruaValida &&
+        numeroValido &&
+        bairroValido
     ) {
-      window.location.href = "alunos.html";
+        window.location.href = "alunos.html";
     } else {
-      alert("Por favor, preencha todos os campos corretamente.");
+        alert("Por favor, preencha todos os campos corretamente.");
     }
-  }
-  
-  
-  const botaoConcluir = document.querySelector("#concluir");
-  botaoConcluir.addEventListener("click", verificarCampos);
-  
+}
+
+
+const botaoConcluir = document.querySelector("#concluir");
+botaoConcluir.addEventListener("click", verificarCampos);
